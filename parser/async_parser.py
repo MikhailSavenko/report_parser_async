c1419f1ec1abe4ba0cb360a2b355baed4de6934e
Михаил Савенко <mi47sav@gmail.com>
2025-02-24 14:51:13 +0300

    цикл + корутина get_page_data и get_urls разделены

--- parser/async_parser.py
+++ parser/async_parser.py
@@ -11,38 +11,46 @@ urls = {
     }
 
 
-async def get_urls(url):
+async def get_page_data(url, session_aiohttp):
+    response = await session_aiohttp.get(url)
+    html = await response.text()
 
-    async with aiohttp.ClientSession() as session_aiohttp:
+    soup = BeautifulSoup(html, features="lxml")
 
-        response = await session_aiohttp.get(url)
-        html = await response.text()
+    pattern = re.compile(r"/upload/reports/oil_xls/.*\.xls")
+    pattern_date = re.compile(r"\b\d{2}\.\d{2}\.\d{4}\b")
 
-        soup = BeautifulSoup(html, features="lxml")
+    date = soup.find_all(string=pattern_date)[:10]
 
-        pattern = re.compile(r"/upload/reports/oil_xls/.*\.xls")
-        pattern_date = re.compile(r"\b\d{2}\.\d{2}\.\d{4}\b")
-
-        date = soup.find_all(string=pattern_date)[:10]
-
-        for idx, link in enumerate(soup.find_all("a", href=pattern)):
-            if int(date[idx][6:]) == 2024:
-                return
-
-            urls["data"].append({
-                "urls": link.get("href"),
-                "date": (date[idx])
-            })
-        # Тут у нас url следующей страницы
-        next_page_tag = soup.select_one("li.bx-pag-next a")
-
-        print("Страница спаршена")
-        if next_page_tag:
-            next_page = next_page_tag.get("href")
-            URL = urljoin(URL_MAIN, next_page)
-            await get_urls(URL)
+    for idx, link in enumerate(soup.find_all("a", href=pattern)):
+        if int(date[idx][6:]) == 2023:
+            print("Последняя Страница спаршена")
             print("Конец")
             print(urls)
+            return
+        urls["data"].append({
+            "urls": link.get("href"),
+            "date": (date[idx])
+        })
+    
+    print("Страница спаршена")
+    # Тут у нас url следующей страницы
+    next_page_tag = soup.select_one("li.bx-pag-next a")
+    if next_page_tag:
+        next_page = next_page_tag.get("href")
+        URL = urljoin(URL_MAIN, next_page)
+        return URL
+
+
+async def get_urls(url):
+    async with aiohttp.ClientSession() as session_aiohttp:
+        pages_to_process = [url]
+
+        while pages_to_process:
+            current_url = pages_to_process.pop(0)
+            next_page = await get_page_data(url=current_url, session_aiohttp=session_aiohttp)
+            if next_page:
+                pages_to_process.append(next_page)
 
 
 async def main():
