44d87d360f33c9ac6b4fd844afeaec5adff70f41
Михаил Савенко <mi47sav@gmail.com>
2025-02-24 17:09:23 +0300

    Вместо цикла, создаем задачу рекурсивнро для некст страницы

--- parser/async_parser.py
+++ parser/async_parser.py
@@ -11,18 +11,19 @@ urls = {
     }
 
 
-async def get_page_data(url, session_aiohttp):
+async def get_urls(url, session_aiohttp):
     response = await session_aiohttp.get(url)
     html = await response.text()
 
     soup = BeautifulSoup(html, features="lxml")
-
+    
     pattern = re.compile(r"/upload/reports/oil_xls/.*\.xls")
     pattern_date = re.compile(r"\b\d{2}\.\d{2}\.\d{4}\b")
 
     date = soup.find_all(string=pattern_date)[:10]
-
-    for idx, link in enumerate(soup.find_all("a", href=pattern)):
+    links = soup.find_all("a", href=pattern)
+    
+    for idx, link in enumerate(links):
         if int(date[idx][6:]) == 2023:
             print("Последняя Страница спаршена")
             print("Конец")
@@ -32,29 +33,18 @@ async def get_page_data(url, session_aiohttp):
             "urls": link.get("href"),
             "date": (date[idx])
         })
-    
     print("Страница спаршена")
-    # Тут у нас url следующей страницы
+
     next_page_tag = soup.select_one("li.bx-pag-next a")
     if next_page_tag:
         next_page = next_page_tag.get("href")
         URL = urljoin(URL_MAIN, next_page)
-        return URL
-
-
-async def get_urls(url):
-    async with aiohttp.ClientSession() as session_aiohttp:
-        pages_to_process = [url]
-
-        while pages_to_process:
-            current_url = pages_to_process.pop(0)
-            next_page = await get_page_data(url=current_url, session_aiohttp=session_aiohttp)
-            if next_page:
-                pages_to_process.append(next_page)
+        await asyncio.create_task(get_urls(URL, session_aiohttp))
 
 
 async def main():
-    await get_urls(URL_WITH_RESULTS)
+    async with aiohttp.ClientSession() as session_aiohttp:
+        await get_urls(URL_WITH_RESULTS, session_aiohttp)
 
 if __name__ == "__main__":
     time0 = time()
