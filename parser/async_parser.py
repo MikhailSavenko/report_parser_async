45e788f7a8f713f851268b1c515e8358108ca5de
Михаил Савенко <mi47sav@gmail.com>
2025-02-25 16:57:20 +0300

    Разделена логика сохранения в БД и парсинга. Добавлены логи к двум методам

--- parser/async_parser.py
+++ parser/async_parser.py
@@ -12,71 +12,10 @@ import pandas as pd
 from datetime import datetime
 from db.models import SpimexTradingResult
 from db.config import AsyncSessionLocal
+import logging
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-async def parse_tags(date, links, queue: asyncio.Queue):
-    for idx, link in enumerate(links):
-        if int(date[idx][6:]) == 2023:
-            print("Последняя Страница спаршена")
-            raise YearComplited
-        await queue.put(link.get("href"))
-    print("Страница спаршена")
-
-
-async def parse_file(file_path: str, session) -> list[SpimexTradingResult]:
-    pattern_date = r'oil_xls_(\d{8})'
-    match_date = re.search(pattern_date, file_path)
-    date_doc = match_date.group(1)
-    date = datetime.strptime(date_doc, '%Y%m%d').date()
-
-    df = pd.read_excel(file_path, header=None)
-    header_row = df[df.apply(lambda row: row.astype(str).str.contains('Метрическая тонна').any(), axis=1)].index[0]
-    res_df = pd.read_excel(file_path, header=header_row+2)
-
-    for index in range(100000):
-        rows = res_df.iloc[index].to_list()
-        if rows[1] in ('Итого:', 'Итого: ', ' Итого:', 'Итого по секции:'):
-            break
-        if pd.isna(rows[4]) or pd.isna(rows[14]) or pd.isna(rows[5]):
-            continue
-        if rows[14] == '-':
-            continue
-        new_oil = SpimexTradingResult(
-            exchange_product_id=str(rows[1]),
-            exchange_product_name=str(rows[2]),
-            oil_id=str(rows[1][:4]),
-            delivery_basis_id=str(rows[1][4:7]),
-            delivery_basis_name=str(rows[3]),
-            delivery_type_id=str(rows[1][-1]),
-            volume=int(rows[4]) if rows[4] != "-" else 0,
-            total=int(rows[5]) if rows[5] != "-" else 0,
-            count=int(rows[14]) if rows[14] != "-" else 0,
-            date=date
-        )
-        session.add(new_oil)
-    await session.commit()
-
-
-async def download_xml(url, session_aiohttp, queue):
-    download = BASE_DIR / 'download'
-    download.mkdir(exist_ok=True)
-    name = url.split('/')[-1].split('?')[0]
-    filename = download / name
-    url_full = urljoin(URL_MAIN, url)
-
-    async with session_aiohttp.get(url_full) as response:
-        if response.status == HTTPStatus.OK:
-            content = await response.read()
-            with open(filename, mode='wb') as f:
-                f.write(content)
-            queue.task_done()
-            return filename
-        else:
-            queue.task_done()
-            return None
-    return None
 
 
 async def get_urls(url, session_aiohttp, queue: asyncio.Queue):
@@ -103,9 +42,101 @@ async def get_urls(url, session_aiohttp, queue: asyncio.Queue):
         await asyncio.create_task(get_urls(URL, session_aiohttp, queue))
 
 
-async def parse_file_with_session(file_path):
+async def parse_tags(date: list, links: list, queue: asyncio.Queue):
+    """
+    Достаем url для скачивания файла
+      date - спиcок дат на странице
+      links - список тега ссылок на файлы
+    """
+    try:
+        for idx, link in enumerate(links):
+            if int(date[idx][6:]) == 2023:
+                raise YearComplited
+            await queue.put(link.get("href"))
+    except YearComplited:
+        raise
+    except ValueError:
+        logging.exception(f"Некорректная дата {date[idx]}. Не было приведено к целому числу.")
+    except IndexError:
+        logging.exception("Ошибка: Индексы в списках date и links не совпадают.")
+
+
+async def download_xml(url, session_aiohttp, queue):
+    download = BASE_DIR / 'download'
+    download.mkdir(exist_ok=True)
+    name = url.split('/')[-1].split('?')[0]
+    filename = download / name
+    url_full = urljoin(URL_MAIN, url)
+
+    async with session_aiohttp.get(url_full) as response:
+        if response.status == HTTPStatus.OK:
+            content = await response.read()
+            with open(filename, mode='wb') as f:
+                f.write(content)
+            queue.task_done()
+            return filename
+        else:
+            queue.task_done()
+            return None
+    return None
+
+
+async def parse_file(file_path: str):
+    """
+    Достаем данные из файла для записи в БД
+      file_path - путь к файлу
+    """
+    try:
+        pattern_date = r'oil_xls_(\d{8})'
+        match_date = re.search(pattern_date, file_path)
+        if not match_date:
+            logging.info(f"Не удалось извлечь дату из имени файла {file_path}")
+            return None
+        date_doc = match_date.group(1)
+        date = datetime.strptime(date_doc, '%Y%m%d').date()
+        
+        df = pd.read_excel(file_path, header=None)
+        mathed_row = df[df.apply(lambda row: row.astype(str).str.contains('Метрическая тонна').any(), axis=1)].index
+        if mathed_row.empty:
+            logging.info(f"Ошибка: 'Метрическая тонна' не найдена в файле {file_path}")
+            return None
+        header_row = mathed_row[0]
+
+        res_df = pd.read_excel(file_path, header=header_row + 2)
+        return date, res_df
+    
+    except Exception as e:
+        logging.exception(f"Ошибка при парсинге файлов в {file_path}: {e}", stack_info=True)
+
+
+async def save_data_to_db(data):
     async with AsyncSessionLocal() as session:
-        await parse_file(file_path, session)
+        await save_in_db(data[0], data[1], session)
+
+    
+async def save_in_db(date, res_df, session):
+    for index in range(100000):
+        rows = res_df.iloc[index].to_list()
+        if rows[1] in ('Итого:', 'Итого: ', ' Итого:', 'Итого по секции:'):
+            break
+        if pd.isna(rows[4]) or pd.isna(rows[14]) or pd.isna(rows[5]):
+            continue
+        if rows[14] == '-':
+            continue
+        new_oil = SpimexTradingResult(
+            exchange_product_id=str(rows[1]),
+            exchange_product_name=str(rows[2]),
+            oil_id=str(rows[1][:4]),
+            delivery_basis_id=str(rows[1][4:7]),
+            delivery_basis_name=str(rows[3]),
+            delivery_type_id=str(rows[1][-1]),
+            volume=int(rows[4]) if rows[4] != "-" else 0,
+            total=int(rows[5]) if rows[5] != "-" else 0,
+            count=int(rows[14]) if rows[14] != "-" else 0,
+            date=date
+        )
+        session.add(new_oil)
+    await session.commit()
 
 
 async def main():
@@ -127,9 +158,17 @@ async def main():
 
         tasks_files = []
         for file_path in download_files:
-            tasks_files.append(asyncio.create_task(parse_file_with_session(str(file_path))))
-        await asyncio.gather(*tasks_files)
+            tasks_files.append(asyncio.create_task(parse_file(str(file_path))))
 
+        data_to_save = await asyncio.gather(*tasks_files)
+
+        tasks_save = []
+        for data in data_to_save:
+            if data is None:
+                continue
+            tasks_save.append(asyncio.create_task(save_data_to_db(data)))
+        await asyncio.gather(*tasks_save)
+        
 
 if __name__ == "__main__":
     time0 = time()
