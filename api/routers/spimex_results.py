e823a2bc2389a0ca1ab38ea4f0d4331e430141fe
Михаил Савенко <mi47sav@gmail.com>
2025-04-06 16:33:58 +0300

    метод обработчик get_trading_results

--- api/routers/spimex_results.py
+++ api/routers/spimex_results.py
@@ -1,5 +1,40 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status, Query
+from typing import Annotated
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
+from api.backend.db_depends import get_db
+from db.models import SpimexTradingResult
+from api.schemas import SpimexTradingResultDB
 
 router = APIRouter(prefix="/results", tags=["results"])
 
 
+@router.get("/", response_model=list[SpimexTradingResultDB])
+async def get_trading_results(db: Annotated[AsyncSession, Depends(get_db)], 
+                              limit: int = Query(10, ge=0, le=10), 
+                              offset: int = Query(0, ge=0),
+                              oil_id: Annotated[str | None, Query()] = None,
+                              delivery_type_id: Annotated[str | None, Query()] = None,
+                              delivery_basis_id: Annotated[str | None, Query()] = None):
+
+    stmp = select(SpimexTradingResult)
+
+    if oil_id:
+        stmp = stmp.where(SpimexTradingResult.oil_id == oil_id)
+
+    if delivery_type_id:
+        stmp = stmp.where(SpimexTradingResult.delivery_type_id == delivery_type_id)
+    
+    if delivery_basis_id:
+        stmp = stmp.where(SpimexTradingResult.delivery_basis_id == delivery_basis_id)
+
+    stmp = stmp.limit(limit).offset(offset)
+
+    results = await db.scalars(stmp)
+    if results is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="There are no trading results!"
+        )
+    return results.all()
