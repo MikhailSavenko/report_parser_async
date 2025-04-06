24de69685a17e90dfff791320462cf334a4ecbb8
Михаил Савенко <mi47sav@gmail.com>
2025-04-06 17:26:04 +0300

    get_dynamics add

--- api/routers/spimex_results.py
+++ api/routers/spimex_results.py
@@ -11,6 +11,38 @@ from api.schemas import SpimexTradingResultDB
 router = APIRouter(prefix="/results", tags=["results"])
 
 
+@router.get("/{start_date}/{end_date}", response_model=list[SpimexTradingResultDB])
+async def get_dynamics(db: Annotated[AsyncSession, Depends(get_db)],
+                       start_date: date, 
+                       end_date: date,
+                       limit: int = Query(10, ge=0, le=10), 
+                       offset: int = Query(0, ge=0),
+                       oil_id: Annotated[str | None, Query()] = None,
+                       delivery_type_id: Annotated[str | None, Query()] = None,
+                       delivery_basis_id: Annotated[str | None, Query()] = None):
+    """Cписок торгов за заданный период"""
+    stmp = select(SpimexTradingResult).where(SpimexTradingResult.date >= start_date, SpimexTradingResult.date <= end_date)
+
+    if oil_id:
+        stmp = stmp.where(SpimexTradingResult.oil_id == oil_id)
+
+    if delivery_type_id:
+        stmp = stmp.where(SpimexTradingResult.delivery_type_id == delivery_type_id)
+    
+    if delivery_basis_id:
+        stmp = stmp.where(SpimexTradingResult.delivery_basis_id == delivery_basis_id)
+        
+    stmp = stmp.limit(limit).offset(offset)
+    
+    results = await db.scalars(stmp)
+    if results is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="There are no trading results!"
+        ) 
+    return results.all()
+
+
 @router.get("/", response_model=list[SpimexTradingResultDB])
 async def get_trading_results(db: Annotated[AsyncSession, Depends(get_db)], 
                               limit: int = Query(10, ge=0, le=10), 
@@ -46,5 +78,12 @@ async def get_trading_results(db: Annotated[AsyncSession, Depends(get_db)],
 async def get_last_trading_dates(db: Annotated[AsyncSession, Depends(get_db)], days: int):
     """Cписок дат последних торговых дней"""
     stmp = select(SpimexTradingResult.date).distinct().order_by(SpimexTradingResult.date.desc()).limit(days)
+    
     dates = await db.scalars(stmp)
+
+    if dates is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="There are no trading dates!"
+        )
     return dates
