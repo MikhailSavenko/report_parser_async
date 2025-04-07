60bb6270b9dbf33c51520473790f542ce9467753
Михаил Савенко <mi47sav@gmail.com>
2025-04-07 19:25:26 +0300

    scheduler добален для очистки кеша + метод clear_cache()

--- api/backend/db_depends.py
+++ api/backend/db_depends.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.backend.db import async_session_maker
 
+
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
--- api/main.py
+++ api/main.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from redis import asyncio as aioredis
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 import os
 from dotenv import load_dotenv
@@ -11,12 +12,23 @@ from api.routers import spimex_results
 
 load_dotenv()
 
+
+scheduler = AsyncIOScheduler()
+
+
+@scheduler.scheduled_job("cron", hour=14, minute=11)
+async def clear_cache():
+    await FastAPICache.clear()
+
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     redis = aioredis.from_url(os.getenv("REDIS"))
     FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
+    scheduler.start()
     yield
     await FastAPICache.clear()
+    await scheduler.shutdown()
 
 app = FastAPI(lifespan=lifespan)
 
