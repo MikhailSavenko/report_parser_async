c1c3b25a60379410dc307d549fd8a37b188c3d5c
Михаил Савенко <mi47sav@gmail.com>
2025-04-30 19:12:38 +0300

    ltd test_404_not_found

--- tests/integration_tests/test_shpimex_result_api.py
+++ tests/integration_tests/test_shpimex_result_api.py
@@ -2,9 +2,9 @@ import pytest
 
 
 @pytest.mark.anyio
-async def test_status_code(async_client):
-    days = 4
+async def test_404_not_found(async_client):
+    days = 0
     response = await async_client.get(f"/results/dates/{days}")
-    assert response.status_code == 200
+    assert response.status_code == 404
 
 
