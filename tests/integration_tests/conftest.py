5ebfa777b839bbf62b6363f2aa4b950bec792de6
Михаил Савенко <mi47sav@gmail.com>
2025-04-30 17:16:42 +0300

    fixture asyio_backend

--- tests/integration_tests/conftest.py
+++ tests/integration_tests/conftest.py
@@ -4,6 +4,11 @@ from api.main import app
 
 
 @pytest.fixture(scope="session")
+def anyio_backend():
+    return "asyncio"
+
+
+@pytest.fixture
 async def async_client():
     async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as client:
         yield client
---
+++ tests/integration_tests/test_shpimex_result_api.py
@@ -0,0 +1,9 @@
+import pytest
+
+
+@pytest.mark.anyio
+async def test_status_code(async_client):
+    days = 4
+    response = await async_client.get(f"/results/dates/{days}")
+    assert response.status_code == 200
+        
