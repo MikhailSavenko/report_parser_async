d28b56966daa22a50063d20353f05e5ed46e87be
Михаил Савенко <mi47sav@gmail.com>
2025-04-30 17:47:09 +0300

    initialize_cache + mock cache decorator

--- tests/integration_tests/conftest.py
+++ tests/integration_tests/conftest.py
@@ -1,6 +1,9 @@
 from httpx import ASGITransport, AsyncClient
 import pytest
 from api.main import app
+from pytest_mock import MockerFixture
+from fastapi_cache import decorator, FastAPICache
+from fastapi_cache.backends.inmemory import InMemoryBackend
 
 
 @pytest.fixture(scope="session")
@@ -12,3 +15,20 @@ def anyio_backend():
 async def async_client():
     async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as client:
         yield client
+
+
+@pytest.fixture
+async def fastapi_cache_mock(mocker: MockerFixture):
+    
+    def no_cache(func):
+        return func
+    
+    mocker.patch.object(decorator, "cache", no_cache)
+    yield
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def initialize_cache():
+    FastAPICache.init(InMemoryBackend(), prefix="test-cache")
+    yield
+    await FastAPICache.clear(namespace="test-cache")
--- tests/integration_tests/test_shpimex_result_api.py
+++ tests/integration_tests/test_shpimex_result_api.py
@@ -2,7 +2,7 @@ import pytest
 
 
 @pytest.mark.anyio
-async def test_status_code(async_client):
+async def test_status_code(async_client, fastapi_cache_mock):
     days = 4
     response = await async_client.get(f"/results/dates/{days}")
     assert response.status_code == 200
