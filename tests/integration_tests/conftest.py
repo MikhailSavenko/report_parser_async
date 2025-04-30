9a4de8249a914140ebef52fe067e0f77a1824a93
Михаил Савенко <mi47sav@gmail.com>
2025-04-30 14:47:07 +0300

    add fixture async_client

---
+++ tests/integration_tests/conftest.py
@@ -0,0 +1,9 @@
+from httpx import ASGITransport, AsyncClient
+import pytest
+from api.main import app
+
+
+@pytest.fixture(scope="session")
+async def async_client():
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as client:
+        yield client
