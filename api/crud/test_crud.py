16e8f13e57a46ad835ebf353062daf3bc68e42c5
Михаил Савенко <mi47sav@gmail.com>
2025-05-01 17:30:54 +0300

    del crud rep

--- api/crud/test_crud.py
+++
@@ -1,9 +0,0 @@
-from .base_crud import CRUDBase
-from db.models import SpimexTradingResult
-
-
-class CrudTestRepository(CRUDBase):
-    pass
-
-
-crud_test_repository = CrudTestRepository(SpimexTradingResult)
